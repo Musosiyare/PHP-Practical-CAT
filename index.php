175e9213274cd02b29eb638a439b96a4a8155176
musosiyare <musosiyarethacien10@gmail.com>
2022-05-30 10:17:50 -0700

    commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <form action="insert_user.php" method="post">
+    <header class="text-white p-1 text-center"style="background-color:blue;">
+<h5>IREMBO | Practical CAT</h5>
+</header>
+
+<!-- form container -->
+<div class="d-flex justify-content-center align-items-center" style="height:80vh;">
+    <!-- <h1>Test</h1> -->
+        <div class="border px-4 pt-4 position-relative text-center w-25 shadow" style="border-right:5px;">
+           <h6><b>Connect to IREMBO</b> </h6><br><br><br>
+           <div class="my-5">
+                <button type="submit" name="facebook" class="form-control btn btn-blue" style="background-color:blue;color:white;font-size:18px; font-weight:bold;">
+                    <i class="fa fa-facebook-official text-white" aria-hidden="true" style="font-size:25px;"></i>
+                    Continue with Facebook
+                </button><br><br><br><br><br>
+           </div>
+        </div>
+</div>
+</form>
+</body>
+</html>
---
+++ loged_out.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <form action="insert_user.php" method="post">
+    <header class="text-white p-1 text-center"style="background-color:blue;">
+<h5>IREMBO | Practical CAT</h5>
+</header>
+
+<!-- form container -->
+<div class="d-flex justify-content-center align-items-center" style="height:80vh;">
+    <!-- <h1>Test</h1> -->
+        <div class="border px-4 pt-4 position-relative text-center w-25 shadow" style="border-right:5px;">
+           <h6><b>Connect to IREMBO</b> </h6>
+           <img src="images/bg2.jpg" class="rounded-circle" style="height:100px; width:100px;"><br>
+           <b>TUYISHIMIRE Peter</b> 
+           peterofficial@gmail.com
+           <div class="my-5">
+                <button type="submit" name="logout" class="form-control btn btn-blue"
+                 style="width:40%;background-color:blue;color:white;font-size:15px;">
+                Log out
+                </button><br><br>
+           </div>
+        </div>
+</div>
+</form>
+</body>
+</html>
